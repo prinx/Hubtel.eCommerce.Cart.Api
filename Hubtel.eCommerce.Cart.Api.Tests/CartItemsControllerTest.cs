9bcdb8cbdded99f49e2d54530c588132564e194e
Prince Dorcis <princedorcis@gmail.com>
2022-07-25 15:32:45 +0000

    Refine test setup

---
+++ Hubtel.eCommerce.Cart.Api.Tests/CartItemsControllerTest.cs
@@ -0,0 +1,46 @@
+﻿using Hubtel.eCommerce.Cart.Api.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hubtel.eCommerce.Cart.Api.Tests
+{
+    public class CartItemsControllerTest : IClassFixture<TestDatabaseFixture>
+    {
+        public TestDatabaseFixture Fixture { get; }
+
+        //private ILogger _logger;
+        public CartItemsControllerTest(TestDatabaseFixture fixture)
+        {
+            Fixture = fixture;
+
+            //var serviceProvider = new ServiceCollection()
+            //    .AddLogging()
+            //    .BuildServiceProvider();
+            //var factory = serviceProvider.GetService<ILoggerFactory>(); 
+            //_logger = factory.CreateLogger<CartItemsControllerTest>();
+        }
+
+        [Fact]
+        public async void GetCartItems_Should_Return_OK()
+        {
+            using var context = Fixture.CreateContext();
+            var mock = new Mock<ILogger<CartItemsController>>();
+            ILogger<CartItemsController>  _logger = mock.Object;
+            //_logger = Mock.Of<ILogger>();
+
+            var controller = new CartItemsController(context, _logger);
+
+            var cartItems = await controller.GetCartItems();
+
+            Console.WriteLine(cartItems);
+
+            Assert.Equal(3, 3);
+        }
+    }
+}
---
+++ Hubtel.eCommerce.Cart.Api.Tests/TestDatabaseFixture.cs
@@ -0,0 +1,46 @@
+﻿using Hubtel.eCommerce.Cart.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hubtel.eCommerce.Cart.Api.Tests
+{
+
+    public class TestDatabaseFixture
+    {
+        private const string ConnectionString = @"Server=localhost;Port=5432;Database=test_hubtel_ecommerce;User Id=postgres;Password=postgres";
+
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public TestDatabaseFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+
+                        context.AddRange();
+                        
+                        context.SaveChanges();
+                    }
+
+                    _databaseInitialized = true;
+                }
+            }
+        }
+
+        public CartContext CreateContext()
+            => new CartContext(
+                new DbContextOptionsBuilder<CartContext>()
+                    .UseNpgsql(ConnectionString)
+                    .Options);
+    }
+}
